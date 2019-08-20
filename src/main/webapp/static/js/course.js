7cfefc267d5c23564b727660a47dea82812065ce
StudyBoy007 <1045112166@qq.com>
2019-08-21 00:05:41 +0800

    second

--- src/main/webapp/static/js/course.js
+++ src/main/webapp/static/js/course.js
@@ -17,5 +17,33 @@ $(function () {
         }))
     })
 
+    // $(".collectAll").mouseover(function () {
+    //     $(this).find(".collect").eq(0).attr("myicon", "collect4");
+    // }).mouseleave(function () {
+    //     $(this).find(".collect").eq(0).attr("myicon", "collect3");
+    // })
+
+    $(".collectAll").click(function () {
+        if ($(this).find(".collect").eq(0).attr("my-icon") == "collect2") {
+            $(this).find(".collect").eq(0).attr("my-icon", "collect1");
+        } else {
+            $(this).find(".collect").eq(0).attr("my-icon", "collect2");
+        }
+    })
+
+    // $(".hook").click(function () {
+    //     if ($(this).attr("my-icon") == "select02") {
+    //         $(this).attr("my-icon", "select01");
+    //         var sum = AllMoney();
+    //         $(".jsAltogether").text(sum);
+    //         juifyAllSelect()
+    //     } else {
+    //         $(this).attr("my-icon", "select02");
+    //         var sum = AllMoney();
+    //         $(".jsAltogether").text(sum);
+    //         juifyAllSelect()
+    //     }
+    // })
+
 
 })
